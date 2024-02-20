478d6ac96a3fb6a472d6ab7ab54e55636291c20a
PodilapuPolinaidu <142189105+PodilapuPolinaidu@users.noreply.github.com>
2024-02-20 20:19:18 +0530

    Add files via upload

---
+++ SwitchStatement.java
@@ -0,0 +1,26 @@
+package Branching;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        char c='a';
+        switch (c){
+            case 'a':
+                System.out.println("Vowel");
+                break;
+            case 'e':
+                System.out.println("Vowel");
+                break;
+            case 'i':
+                System.out.println("Vowel");
+                break;
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+        }
+    }
+}
